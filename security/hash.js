5850cde71656099d8f4e0825ad331c94168d0bc3
Pavan-Kumar-KN <pavankumarkn715@gmail.com>
2025-11-25 19:41:07 +0530

    feat(hash): create , compare function are added

---
+++ security/hash.js
@@ -0,0 +1,31 @@
+import { createHash, timingSafeEqual } from 'node:crypto';
+import { validateApiKey } from '../helper/validateKey';
+
+// hash the api key then store in the db
+function hashApiKey(key) {
+    return createHash("sha256").update(key).digest("hex");
+}
+
+export function compareHashedApiKey(rawKey, storedHash) {
+
+    let isValidkey = validateApiKey(rawKey);
+
+    if(!isValidkey){
+        return;
+    }
+    
+    const hashed = hashApiKey(rawKey);
+
+    const buffA = Buffer.from(hashed);
+    const buffB = Buffer.from(storedHash);
+
+    if (buffA.length !== buffB.length) return false;
+
+    return timingSafeEqual(buffA, buffB);
+}
+
+
+export {
+    hashApiKey,
+    compareHashedApiKey
+}
